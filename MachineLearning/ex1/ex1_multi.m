a02600d48b2151bc82fd5fccff11a379fe277262
rfrazho <rfrazho@mitre.org>
2018-06-30 15:31:07 -0400

    Multi Variable Gradient Descent completed.

--- MachineLearning/ex1/ex1_multi.m
+++ MachineLearning/ex1/ex1_multi.m
@@ -104,8 +104,9 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
-
+x_dat = [1650 3];
+x_norm = [1 (x_dat - mu)./sigma]; % normalize
+price = theta.' * x_norm.';
 
 % ============================================================
 
@@ -149,7 +150,8 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+x = [1 1650 3];
+price = theta.' * x.';
 
 
 % ============================================================
--- MachineLearning/ex1/gradientDescentMulti.m
+++ MachineLearning/ex1/gradientDescentMulti.m
@@ -8,30 +8,32 @@ m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
-
+    
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
+    %               theta.
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-
-
-
-
-
-
-
-
-
-
+    
+    h = X*theta;
+    theta = theta - (alpha/m).*(X.'*(h-y));
+    %     for jj = 1:length(theta)
+    %        sum_j = sum(h-y)
+    %     end
+    %     sum1 = sum(h-y);
+    %     sum2 = sum((h-y).*X(:,2));
+    %     theta(1) = theta(1) - (alpha/m)*sum1;
+    %     theta(2) = theta(2) - (alpha/m)*sum2;
+    
+    
+    
     % ============================================================
-
-    % Save the cost J in every iteration    
+    
+    % Save the cost J in every iteration
     J_history(iter) = computeCostMulti(X, y, theta);
-
+    
 end
 
 end
